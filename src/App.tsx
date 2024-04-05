1a643416aa625b349440e7fa1a84c2752361e455
Menna Mohamed <79084467+MennaAnwar@users.noreply.github.com>
2024-04-05 23:06:22 +0200

    opening to be continued

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import Cursor from "./Components/Cursor/Cursor";
+import Intro from "./Components/Intro/Intro";
 
 function App() {
   return (
     <div className="App">
       <Cursor />
+      <Intro />
     </div>
   );
 }
